65ffc98a2c38da9ab228263aafd4ea750b87ae53
Rifqi Maula Iqbal <rifqimaulaiqbal@gmail.com>
2016-06-16 18:17:34 +0700

    Add files via upload

---
+++ public/css/coba.js
@@ -0,0 +1,10 @@
+
+    // my custom script
+function doReload(catid){
+	document.location = 'barang/create' + catid;
+	
+	/* But if you want to submit the form just comment above line and uncomment following lines*/
+	//document.frm1.action = 'samepage.php';
+	//document.frm1.method = 'post';
+	//document.frm1.submit();
+
